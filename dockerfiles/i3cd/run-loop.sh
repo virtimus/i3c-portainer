527d18f13c4d476911f4372d92d4b90fc172c8bb
virtimus <virtimus@gmail.com>
2018-01-01 12:19:38 +0100

    Update run-loop.sh

--- dockerfiles/i3cd/run-loop.sh
+++ dockerfiles/i3cd/run-loop.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 #/usr/bin/supervisord
 #/usr/bin/supervisorctl
+
+if [ ! -f /i3c/i3c.sh ]; then  
+    cd / 
+    git clone https://github.com/virtimus/i3c.git
+    cd /i3c
+    npm install grunt
+    npm install -g grunt-cli
+    npm install -g grunt
+	
+    cd /i3c
+    yarn install     
+fi
+
+    
 while ( true )
     do
     #//echo "Detach with Ctrl-p Ctrl-q. Dropping to shell"
