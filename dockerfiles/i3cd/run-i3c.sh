a094d99427b1f7fc494862480852e6466b871a55
virtimus <virtimus@gmail.com>
2018-01-01 19:20:48 +0100

    Update run-i3c.sh

--- dockerfiles/i3cd/run-i3c.sh
+++ dockerfiles/i3cd/run-i3c.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+
+if [ ! -f /i3c/i3c.sh ]; then  
+    cd / 
+    git clone https://github.com/virtimus/i3c.git
+    
+    cd /i3c
+    chmod -R a+x **/*.sh
+fi
+
+cd /i3c
+npm install grunt
+npm install -g grunt-cli
+npm install -g grunt
+	
+cd /i3c
+yarn install 
+
 cd /i3c 
-grunt run-dev >> dist/grunt-run-dev.log &
+grunt run-dev >> /log/grunt-run-dev.log &
