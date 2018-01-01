39525122e22e825d0498c0442ae133ab2842f388
virtimus <virtimus@gmail.com>
2018-01-01 13:48:29 +0100

    Update bootstrap.sh

--- i3c-install/bootstrap.sh
+++ i3c-install/bootstrap.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+CONTAINER=i3cd
+RUNNING=$(docker inspect --format="{{.State.Running}}" $CONTAINER 2> /dev/null)
+
+#if [ $? -eq 1 ]; then
+#  echo "UNKNOWN - $CONTAINER does not exist."
+#  exit 3
+#fi
+ 
+if [ "$RUNNING" == "true" ]; then
+    echo "CRITICAL - $CONTAINER is running."
+    exit 0
+fi
+
 ln -s /i3c/i3c.sh /i
 
 /i rebuild i3cd
