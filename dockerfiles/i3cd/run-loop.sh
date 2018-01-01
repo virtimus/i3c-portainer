49f2f84a798dac7c3e690ee8648a1b74cd371831
virtimus <virtimus@gmail.com>
2018-01-01 18:56:48 +0100

    Update run-loop.sh

--- dockerfiles/i3cd/run-loop.sh
+++ dockerfiles/i3cd/run-loop.sh
@@ -5,18 +5,18 @@
 if [ ! -f /i3c/i3c.sh ]; then  
     cd / 
     git clone https://github.com/virtimus/i3c.git
-    cd /i3c
-    npm install grunt
-    npm install -g grunt-cli
-    npm install -g grunt
-	
-    cd /i3c
-    yarn install 
     
     cd /i3c
     chmod -R a+x **/*.sh
 fi
 
+cd /i3c
+npm install grunt
+npm install -g grunt-cli
+npm install -g grunt
+	
+cd /i3c
+yarn install 
     
 while ( true )
     do
