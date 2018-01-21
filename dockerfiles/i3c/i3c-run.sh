da779c07d8fd16b2d358fdf710ce67833c788aeb
virtimus <virtimus@gmail.com>
2018-01-21 09:46:24 +0000

    i3c fix

--- dockerfiles/i3c/i3c-run.sh
+++ dockerfiles/i3c/i3c-run.sh
@@ -2,14 +2,15 @@
 #-v /tmp/portainer:/data \
 #-v /tmp/portainer:/data \
 #-v $i3cDataDir/i3c:/data \
+#	-v /tmp/portainer:/data \
+#-d /data
 commandwp = ${@:2};
 if [ "x$commandwp" = "x" ]; then	
-	commandwp='/app/portainer-linux-amd64 --no-analytics -d /data';	
+	commandwp='/app/portainer-linux-amd64 --no-analytics';	
 fi
 
 docker run -d --name i3c \
 	-p 9000:9000 \
-	-v /tmp/portainer:/data \
 	-v $i3cDataDir/i3c:/data2 \
 	-v $i3cHome:/i3c \
 	-v $i3cLogDir/i3c:/log \
